f50074243de1c602d4a888e7af6f90d33b95a858
Jaseincyberspace <jase.norton@gmail.com>
2022-05-25 01:46:33 +1200

    Renamed package from BIT707_A1... to BIT707_A3...

--- app/src/main/java/BIT707_A3_5001428_ToDoList/App.java
+++ app/src/main/java/BIT707_A3_5001428_ToDoList/App.java
@@ -1,4 +1,11 @@
-package BIT707_A1_5001428_ToDoList;
+/*
+ * BIT707 - Assignment 3
+ * Jason Norton - 5001428
+ */
+package BIT707_A3_5001428_ToDoList;
+import java.sql.*;
+import java.util.ArrayList;
+
 /**
  * ToDo List Application
  * @author Jason Norton
@@ -8,6 +15,7 @@ public class App {
      * The application entry point
      * @param args An array of command-line arguments (Strings)
      */
+    
     public static void main(String[] args) {
         /**
          * Instantiates database connection
@@ -18,6 +26,6 @@ public class App {
         /**
          * Queries the database
          */
-        dbConnection.selectAllFromTaskTable();       
-    }       
+        dbConnection.selectAllFromTaskTable();           
+    }
 }
