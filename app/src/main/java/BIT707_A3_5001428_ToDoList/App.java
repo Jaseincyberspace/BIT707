cea23512df3593c6f9c4624749b2a1d0ff413951
Jaseincyberspace <jase.norton@gmail.com>
2022-06-02 07:33:46 +1200

    Added Calendar View functionality and strikethrough for completed tasks
    TODO: 
    - Fix bug where tasks added do not update their index in the data model 
    which makes 'Mark completed' button mark the wrong task as completed
    - Consider hiding checkboxes for day of week headers in calendar view 
    or establishing functionality to select all tasks on that day and mark 
    them as completed
    - Consider whether delete button should remove all tasks marked as
    complete or just the currently selected one

--- app/src/main/java/BIT707_A3_5001428_ToDoList/App.java
+++ app/src/main/java/BIT707_A3_5001428_ToDoList/App.java
@@ -3,8 +3,6 @@
  * Jason Norton - 5001428
  */
 package BIT707_A3_5001428_ToDoList;
-import java.sql.*;
-import java.util.ArrayList;
 
 /**
  * ToDo List Application
@@ -18,29 +16,6 @@ public class App {
     public static Controller controller = new Controller();
         
     public static void main(String[] args) {
-        /**
-         * Instantiates database connection
-         
-        DbConnection dbConnection = new DbConnection();
-        dbConnection.initDatabase();
-        
-        /**
-         * Queries the database
-         
-        dbConnection.selectAllFromTaskTable(); 
-        
-        /**
-         * Displays MainForm
-         
-        MainForm mainForm = new MainForm();
-        mainForm.setLocationRelativeTo(null);
-        mainForm.pack();
-        mainForm.setVisible(true);
-        
-        ListViewForm listViewForm = new ListViewForm();
-        listViewForm.pack();
-        listViewForm.setVisible(true);
-        */
         controller.run();
     }
 }
