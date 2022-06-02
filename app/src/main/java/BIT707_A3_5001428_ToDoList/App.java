322e1d568092cc96a675214c6f3d1653c9805c9e
Jaseincyberspace <jase.norton@gmail.com>
2022-06-03 02:45:02 +1200

    Tasks marked as complete now have strikethrough when application starts - Added javaDoc comments throughout codebase
    TODO:
    Refactor where applicable to make code more readable and efficient

--- app/src/main/java/BIT707_A3_5001428_ToDoList/App.java
+++ app/src/main/java/BIT707_A3_5001428_ToDoList/App.java
@@ -4,17 +4,12 @@
  */
 package BIT707_A3_5001428_ToDoList;
 
-/**
- * ToDo List Application
- * @author Jason Norton
- */
 public class App {
     /**
      * The application entry point
      * @param args An array of command-line arguments (Strings)
      */
-    public static Controller controller = new Controller();
-        
+    public static Controller controller = new Controller();      
     public static void main(String[] args) {
         controller.run();
     }
